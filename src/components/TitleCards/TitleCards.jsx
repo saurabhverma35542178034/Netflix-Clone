c1afc7a113d1ae5b7eadc4a3599e3e636380a237
Saurabh Verma <saurabhverma3554@gmail.com>
2026-03-16 23:29:16 +0530

    Horizontal Scrolling

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Title.css'
 import cards_data from '../../assets/cards/Cards_data'
 
+
+
 const TitleCards = () => {
+    const handleWheel = (e) => {
+        e.preventDefault();
+        cardsRef.current.scrollLeft += e.deltaY;
+    }
+    const cardsRef = useRef();
+    useEffect(() => { cardsRef.current.addEventListener('wheel', handleWheel), [] });
+    
   return (
     <div className='title-cards'>
           <h2>Popular on Netflix</h2>
-          <div className="card-list">
+          <div className="card-list" ref={ cardsRef}>
               {cards_data.map((card,index) => {
                   return <div className="card" key={index}>
                       <img src={card.image} alt="" />
