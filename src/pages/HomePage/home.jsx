262463aa05d24b06e94f9eb52f59b24467047019
Saurabh Verma <saurabhverma3554@gmail.com>
2026-03-16 16:17:20 +0530

    More Changes

--- src/pages/HomePage/home.jsx
+++ src/pages/HomePage/home.jsx
@@ -5,6 +5,7 @@ import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
+import TitleCards from '../../components/TitleCards/TitleCards'
 const home = () => {
   return (
     <div className='home'>
@@ -16,9 +17,12 @@ const home = () => {
                   <p>Man Surviving in the World of Vampire. He lost his love and seeking redemption.</p>
                   <div className="hero-btns">
                       <button className='btn'><img src={play_icon} alt=""/>Play</button>
-                     <button className='btn dark-btn'>More Info<img src={info_icon} alt="" /></button>
+                      <button className='btn dark-btn'><img src={info_icon}
+                          alt="" />More Info</button>
                   </div>
+                  <TitleCards />
               </div>
+              
 
           </div>
     </div>
