62136092f3c8899d12433237bab44662d96577b3
Saurabh Verma <saurabhverma3554@gmail.com>
2026-03-16 13:04:34 +0530

    Changes in Home.jsx

--- src/pages/HomePage/home.jsx
+++ src/pages/HomePage/home.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
-
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 const home = () => {
   return (
     <div className='home'>
-          <h1>Home</h1>
-          <Navbar/>
+          <Navbar />
+          <div className="hero">
+              <img src={hero_banner} alt="" className='banner-img'/>
+              <div className="hero-caption">
+                  <img src={hero_title} alt="" className='caption-img' />
+                  <p>Man Surviving in the World of Vampire. He lost his love and seeking redemption.</p>
+                  <div className="hero-btns">
+                      <button className='btn'><img src={play_icon} alt=""/>Play</button>
+                     <button className='btn dark-btn'>More Info<img src={info_icon} alt="" /></button>
+                  </div>
+              </div>
+
+          </div>
     </div>
   )
 }
